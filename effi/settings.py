8185502a1f2967345e50b87c8c62a2553b4931d3
tauhid621 <tauhidanjum@gmail.com>
2017-03-19 13:40:31 +0000

    commmiting 8th

--- effi/settings.py
+++ effi/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'website/static'),
+    os.path.join(BASE_DIR, 'website/static'),
 )
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
